64f2500d3f25a960af23fe49fc9ec3870a8533d1
Philip Breau <philip.breau@icesoft.com>
2013-10-21 10:17:58 -0300

    revert removing ice namespace check

--- javascript/bridgeit.js
+++ javascript/bridgeit.js
@@ -12,6 +12,9 @@
  * express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+if (!window['ice']) {
+    window.ice = {};
+}
 if (!window['bridgeit']) {
     window.bridgeit = {};
     window.bridgeIt = window.bridgeit; //alias bridgeit and bridgeIt
