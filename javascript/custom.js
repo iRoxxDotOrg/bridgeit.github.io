851d5a555e9b31409dc6c1775f3ba0aa488fa69e
cruzmiraback <cmiraback@icesoft.com>
2014-01-10 15:30:33 -0700

    Updated YouTube video & fixed IE9 bug.
    - Changed embedded YouTube video to an image that will play on click
    - Fixed carousel bug in IE9 where the carousel rendered with a small
    width

--- javascript/custom.js
+++ javascript/custom.js
@@ -12,3 +12,14 @@ function replaceFeatureText() {
 }
 $(document).ready(replaceFeatureText);
 $(window).resize(replaceFeatureText);
+
+// Replace image with youtube video on click
+$(document).ready(function() {
+    $(".getting-started-video img").click(function(e) {
+        e.preventDefault();
+        var imgWidth = $(".getting-started-video img").width();
+        var imgHeight = $(".getting-started-video img").height();
+        $(".getting-started-video").html
+        ("<iframe width='" + imgWidth + "' height='" + imgHeight + "' src='http://www.youtube.com/embed/W6rAjNIQIdA?vq=large&autoplay=1&rel=0' frameborder='0' allowfullscreen></iframe>");
+    });
+});
