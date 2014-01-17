2e4ac83905904fb9c203f9b44b7e76ade385518c
cruzmiraback <cmiraback@icesoft.com>
2014-01-17 11:39:39 -0700

    Added mobile demo video.
    - Implemented with HTML5 video tag. Used conditional rendering of set
    width/height video tags instead of setting the video size via CSS. This
    is to prevent a bug from occurring on iOS6 or older devices when the
    video size is set in CSS with width/height.

--- javascript/custom.js
+++ javascript/custom.js
@@ -23,3 +23,36 @@ $(document).ready(function() {
         ("<iframe width='" + imgWidth + "' height='" + imgHeight + "' src='http://www.youtube.com/embed/W6rAjNIQIdA?vq=large&autoplay=1&rel=0' frameborder='0' allowfullscreen></iframe>");
     });
 });
+
+//**************** Mobile Demo Video ****************
+
+// Helper method to find the correct video to play depending on the viewport size
+function findCurrentVideo() {
+    var video = null;
+    $(".mobile-demo-video").each(function(){
+        if($(this).is(':visible')) {
+            var id = $(this).children('video').attr('id');
+            video = document.getElementById(id);
+        }
+    })
+    return video;
+}
+
+// When video ends go back to beginning so device is shown
+$(document).ready(function() {
+    var video = findCurrentVideo();
+    if (video) {
+        video.addEventListener('ended',function(){
+            video.currentTime = 0;
+            video.pause();
+        });
+    }
+});
+
+// Play video. Used for the link on the demo page
+function playMobileDemoVideo() {
+    var video = findCurrentVideo();
+    video.play();
+}
+
+//***************************************************
